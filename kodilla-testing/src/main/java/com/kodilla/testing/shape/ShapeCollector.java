aae07edd945203af425641eab05aa8136c5f8f22
IngaPanczyk <ingawaaa@gmail.com>
2020-06-13 09:45:05 +0200

    Zadanie 3.2

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -3,24 +3,22 @@ package com.kodilla.testing.shape;
 import java.util.ArrayList;
 
 public class ShapeCollector {
+    ArrayList<Shape> shapeCollection = new ArrayList<>();
+    Shape shape = new Shape();
 
     public ArrayList addFigure(Shape shape){
-        ArrayList<Shape> shapeCollection = new ArrayList<>();
         shapeCollection.add(shape);
         return shapeCollection;
     }
     public ArrayList removeFigure(int n) {
-        ArrayList<Shape> shapeCollection = new ArrayList<>();
         shapeCollection.remove(n);
         return shapeCollection;
     }
     public Shape getFigure(int n) {
-        ArrayList<Shape> shapeCollection = new ArrayList<>();
         shapeCollection.get(n);
         return shapeCollection.get(n);
     }
     public ArrayList showFigures(){
-        ArrayList<Shape> shapeCollection = new ArrayList<>();
         return shapeCollection;
 
     }
